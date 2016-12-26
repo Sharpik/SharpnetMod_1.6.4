9ee19cb19fd3e5c404c408e4b05e6d891f7a009b
Sharpik <hack3@seznam.cz>
2016-12-26 20:19:01 +0100

    Update 0106
    Add: coments

--- src/ModSharpnet/ShWorldGen.java
+++ src/ModSharpnet/ShWorldGen.java
@@ -31,13 +31,19 @@ public class ShWorldGen implements IWorldGenerator
     
     private void OverworldGen(World world, int chunkX, int chunkZ, Random random)
     {
+        //Návod jak použít funkci pro spawnování rostlin:
+        //FlowerSpawner(world, chunkX, chunkZ, random, <ZDE NÁZEV PROMĚNNÉ S ID BLOKU>, <Sem se dává meta ID (to je to druhé v eshopu za dvojtečkou)>, <minimální výška, kde se bude rostlina spawnovat>, <maximální výška, kde se bude rostlina spawnovat>, <šance že se rostlinka naspawnuje (klasicky 1)>, <zde se nastavuje zda je toto nastavená aktivní (true) nebo neaktivní (false)>);
         
+            //Zde je popsáno v jakém biotopu se bude rostlinka spawnovat (Forest)
             if(BiomeDictionary.isBiomeOfType(this.biome, Type.FOREST))
             {
                 this.biome_type = "FOREST";
                 
+                //Zde se kontroluje zda je v konfiguračním souboru povoleno spawnování Sharpneťáckých rostlinek do mapy
                 if(ModSharpnet.WorldGenFlowers)
                 {
+                    //Sem příjdou funkce pro spawnování rostlin do lesa
+                    
                     //Daffodil Flower
                         FlowerSpawner(world, chunkX, chunkZ, random, SharpnetFlowerBlocks1ID, 0, 64, 128, 1, true);
                     //Lacustrine
@@ -51,6 +57,12 @@ public class ShWorldGen implements IWorldGenerator
             if(BiomeDictionary.isBiomeOfType(this.biome, Type.PLAINS))
             {
                 this.biome_type = "PLAINS";
+                
+                if(ModSharpnet.WorldGenFlowers)
+                {
+                    //Sem příjdou rostlinky, které se mají spawnovat na pláních
+                    
+                }          
             }
             if(BiomeDictionary.isBiomeOfType(this.biome, Type.JUNGLE))
             {
@@ -72,20 +84,35 @@ public class ShWorldGen implements IWorldGenerator
             {
                 this.biome_type = "DESERT";
                 
+                if(ModSharpnet.WorldGenFlowers)
+                {
+                    //Sem příjdou rostlinky, které se mají spawnovat na poušti
+                    
+                }  
+                
+                //Zde se kontroluje zda je v konfiguračním souboru povoleno spawnování Sharpneťáckých rud
                 if(ModSharpnet.WorldGenOres)
                 {
+                    //Toto je příklad spawnování Glowstonu (89) do Stonu(1) ve výšce od 48 do 64 s šancí na naspawnování až 8 Glowstonů
                     OreSpawner(world, chunkX, chunkZ, random, 1, 89, 0, 48, 64, 8, 10, true);
                 }
             }
             if(BiomeDictionary.isBiomeOfType(this.biome, Type.HILLS))
             {
                 this.biome_type = "HILLS";
+                
+                if(ModSharpnet.WorldGenFlowers)
+                {
+                    //Sem příjdou rostlinky, které se mají spawnovat v kopcích
+                    
+                }  
             }
             //debug
             //System.out.println("ShDebug: biome: " + biome_type);
             
     }
     
+    //Funkce pro spawnování rud
     private void OreSpawner(World world, int chunkX, int chunkZ, Random random, int targetBlockID, int blockID, int blockMeta, int minY, int maxY, int veinSize, int chanceToSpawn, boolean turnedOn)
     {
         if(turnedOn)
@@ -101,6 +128,7 @@ public class ShWorldGen implements IWorldGenerator
         }
     }
     
+    //Funkce pro spawnování rostlin
     private void FlowerSpawner(World world, int chunkX, int chunkZ, Random random, int blockID, int blockMeta, int minY, int maxY, int chanceToSpawn, boolean turnedOn)
     {
         if(turnedOn)
