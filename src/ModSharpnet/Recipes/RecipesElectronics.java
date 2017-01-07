20e6f8a3863c378b3e166069ae2d69726537e764
Sharpik <hack3@seznam.cz>
2017-01-07 11:15:39 +0100

    Update 0117
    Recipes fix

--- src/ModSharpnet/Recipes/RecipesElectronics.java
+++ src/ModSharpnet/Recipes/RecipesElectronics.java
@@ -12,7 +12,7 @@ public class RecipesElectronics
     public static void register()
     {
         
-        //Recipe Cofee Machine
+        //Recipe Mixer
         GameRegistry.addRecipe(new ItemStack(Blocks.electro1_block,1,0), new Object[]
         {
             "WWW",
@@ -30,13 +30,13 @@ public class RecipesElectronics
             'W', new ItemStack(Block.cloth,1,15), 'B', new ItemStack(ic2.api.item.Items.getItem("reBattery").getItem(),1,0), 'C', new ItemStack(ic2.api.item.Items.getItem("electronicCircuit").getItem(),1,0)
         });
         
-        //Recipe Mixer
-        GameRegistry.addRecipe(new ItemStack(Blocks.electro1_block,1,1), new Object[]
+        //Recipe Kettle
+        GameRegistry.addRecipe(new ItemStack(Blocks.electro1_block,1,2), new Object[]
         {
             "W W",
             "WBW",
             "WCW",
-            'W', new ItemStack(Block.cloth,1,15), 'B', new ItemStack(ic2.api.item.Items.getItem("reBattery").getItem(),1,0), 'C', new ItemStack(ic2.api.item.Items.getItem("ironCableBlock").getItem(),1,5)
+            'W', new ItemStack(Block.cloth,1,15), 'B', new ItemStack(ic2.api.item.Items.getItem("reBattery").getItem(),1,0), 'C', new ItemStack(ic2.api.item.Items.getItem("ironCableItem").getItem(),1,5)
         });
         
         //Reload for Throwel
